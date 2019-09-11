371f04bec5429033332c0692e45c13eb149badd4
xuyz <987171135@qq.com>
2019-09-11 10:44:12 +0800

    deal with logInfo

--- face-permission-web/src/test/java/com/example/demo/DemoApplicationTests.java
+++ face-permission-web/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import com.face.permission.server.Application;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -10,9 +12,15 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest(classes = Application.class)
 public class DemoApplicationTests {
 
+//    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Test
     public void contextLoads() {
-
+//        logger.trace("I am trace log.");
+//        logger.debug("I am debug log.");
+//        logger.warn("I am warn log.");
+//        logger.error("I am error log.");
+//        logger.info("I am info log.");
     }
 
 }
